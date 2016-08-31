39ee830854546e5b8373cb9d00ac00596174508f
Roger Nell <rnell@ecosys.net>
2016-08-31 14:03:01 -0400

    A dummy app

---
+++ app/src/main/com/nell/App.java
@@ -0,0 +1,12 @@
+package com.nell;
+
+/**
+ * @author rnell
+ * @date 8/31/2016
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World" );
+        System.exit( 0 );
+    }
+}
